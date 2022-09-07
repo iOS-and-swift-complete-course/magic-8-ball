d1acf33f7a98df52b084a6624ee7f831f413a2eb
Chris Hand <cj.hand@gmail.com>
2022-09-07 09:12:01 -0700

    Implement the code I'm sure there is a better way, but this will work for now.

--- magic 8 ball/ViewController.swift
+++ magic 8 ball/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var askButton: UIButton!
+    @IBOutlet weak var ball: UIImageView!
+    private let balls = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func askClicked(_ sender: Any) {
+        askButton.isEnabled = false
+        for i in 0...9 {
+            Timer.scheduledTimer(timeInterval: TimeInterval(Double(i) * 0.25), target: self, selector: #selector(showRandom), userInfo: nil, repeats: false)
+        }
+        
+        Timer.scheduledTimer(timeInterval: TimeInterval(10.0 * 0.25), target: self, selector: #selector(enableButton), userInfo: nil, repeats: false)
+    }
+    
+    @objc
+    private func showRandom() {
+        let selected = Int.random(in: 0...4)
+        ball.image = UIImage(named: balls[selected])
+        print(selected)
+    }
+    
+    @objc
+    private func enableButton() {
+        askButton.isEnabled = true
+        print("enabled")
+    }
 }
 
